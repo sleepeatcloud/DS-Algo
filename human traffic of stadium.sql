7f1fea06cef6a4fd67fdacc5f5652bb7e8eb2011
sleepeatazure <sleepeatazure@gmail.com>
2023-01-07 06:37:15 -0500

    test

--- human traffic of stadium.sql
+++ human traffic of stadium.sql
@@ -4,8 +4,8 @@ select *,ROW_NUMBER() OVER (order by visit_date) rn,
 id-ROW_NUMBER() OVER (order by visit_date) grp
 from lc.stadium
 where no_of_people>100) 
---select * from group_num
---where grp in(
+select * from group_num
+where grp in(
 select grp
 from group_num
 group by grp
